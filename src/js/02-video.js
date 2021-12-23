c18bb3068fdb9a101a6e48ecc18ccc5db500dff0
Vladislav220294 <vladislav22021994@gmail.com>
2021-12-23 04:27:14 +0300

    добавляет вторую задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+// import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(savedTime).then(function (seconds) {
+  if (savedTime) {
+    seconds = savedTime;
+  }
+});
